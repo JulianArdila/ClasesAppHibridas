9caf33e77c297fb95042e95697a1bd610358ee7b
juliaDavid <jdavidardilar@gmail.com>
2021-03-15 18:25:08 -0500

    Agregar Curso admin

--- backend/backendHibrida/alumnos/admin.py
+++ backend/backendHibrida/alumnos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from alumnos.models import Alumno
+from alumnos.models import Alumno, Curso
 
 # Register your models here.
 
 admin.site.register(Alumno)
+
+admin.site.register(Curso)
